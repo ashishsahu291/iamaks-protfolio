3495997771a9a2c8b8800a12a13ac13a3db198c1
Ashish <ashishsahu291@gmail.com>
2025-12-25 20:58:05 +0530

    env.ts file modification

--- sanity/env.ts
+++ sanity/env.ts
@@ -2,18 +2,18 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2025-11-25";
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
+  process.env.SANITY_DATASET ?? process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "SANITY_PROJECT_DATASET or NEXT_PUBLIC_SANITY_PROJECT_DATASET"
 );
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
+  process.env.SANITY_PROJECT_ID ?? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "SANITY_PROJECT_ID or NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
+function assertValue<T>(v: T | undefined, name: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage);
+    throw new Error(`Missing environment variable: ${name}`);
   }
 
   return v;
